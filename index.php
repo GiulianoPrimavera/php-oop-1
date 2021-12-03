6c31f93484f46ebe84f38779f7515992f59c4ba4
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-12-03 09:41:08 +0100

    adds  array containing movies and prints it

--- index.php
+++ index.php
@@ -1,15 +1,24 @@
 <?php 
 require_once "./classes/Movie.php";
 
+$arrMovies = [];
+
 $avengers = new Movie("The Avengers", "English", "9");
 $spirit = new Movie("Spirit wild stallion", null, "9");
 $forrestGump = new Movie("Forrest Gump", "English(USA)", 9);
 // var_dump($avengers);
 
-echo $avengers->buildCover();
-echo "<br>";
-echo $spirit->buildCover();
-echo "<br>";
-echo $forrestGump->buildCover();
+// echo $avengers->buildCover();
+// echo "<br>";
+// echo $spirit->buildCover();
+// echo "<br>";
+// echo $forrestGump->buildCover();
+
+array_push($arrMovies, $avengers, $forrestGump, $spirit);
+// var_dump($arrMovies);
+
+foreach($arrMovies as $movie){
+    echo $movie->buildCover() . "<br>";
+}
 
 ?>
