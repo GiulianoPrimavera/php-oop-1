2ad5bec95dfd4e3edb228f0d6317abf35bd4b6bc
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-12-03 10:45:53 +0100

    uses set functions to set the parameters

--- classes/Movie.php
+++ classes/Movie.php
@@ -6,24 +6,35 @@ class Movie {
 
     public $cover;
 
-    function __construct ($_title = null, $_language = null, $_votes = null){
+    function __construct ($_title = null, $_language = null, $_votes = null){        
+        $this->setTitle($_title);
+        
+        $this->setLanguage($_language);
+
+        $this->setvotes($_votes);
+    }
+
+    public function setTitle($value){
         //controllo se il titolo inserito è numero o stringa
-        if(is_string($_title)){
-            $this->title = $_title;
+        if(is_string($value)){
+            $this->title = $value;
         }else{
             $this->title = null; 
         }
-        
+    }
+
+    public function setLanguage($value){
         //controllo se la lingua inserita è numero o stringa
-        if(is_string($_language)){
-            $this->language = $_language;
+        if(is_string($value)){
+            $this->language = $value;
         }else{
             $this->language = null; 
         }
+    }
 
-
-        if(is_numeric($_votes)){
-            $this->votes = $_votes;
+    public function setvotes($value){
+        if(is_numeric($value)){
+            $this->votes = $value;
         }else{
             $this->votes = null;
         }
